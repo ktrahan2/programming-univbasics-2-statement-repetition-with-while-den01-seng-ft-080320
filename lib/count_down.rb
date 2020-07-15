6909da12b0af0454df49145068a948a8d4de4870
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-15 19:17:08 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,10 +1,6 @@
 count = 10
-while count < 10 do
-  break if count == 1
-  count = count - 1
+while count >= 1 do
+  puts count
+  count -= 1
 end
-if count == 1
-  puts "Happy New Year!"
-else
-  puts "#{count}"
-end
+puts "Happy New Year!"
